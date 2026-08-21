ec951342428d3e5f2fe9f02e673c7d6ef0c5ae11
PhantomHive <yvantebili@gmail.com>
2026-08-21 13:16:09 +0000

    feat: correction du script

--- script.js
+++ script.js
@@ -2,18 +2,22 @@ const taskInput = document.getElementById('taskInput');
 const addTaskBtn = document.getElementById('addTaskBtn');
 const taskList = document.getElementById('taskList');
 
-addTaskBtn.addEventListener('click', () => {
-    const taskText = taskInput.Value.trim();
+addTaskBtn.addEventListener('click', (event) => {
+    event.preventDefault(); // Empêche tout rechargement ou modification bizarre de l'URL
+    
+    // On s'assure de bien cibler .value
+    const taskText = taskInput.value.trim();
+    
     if (taskText !== "") {
         const li = document.createElement('li');
-        li.textContent = taskText ;
-
-        //permet de supprimer la tache au clic
-        li.addEventListener('click' , () => {
+        li.textContent = taskText;
+        
+        // Permet de supprimer la tâche au clic
+        li.addEventListener('click', () => {
             li.remove();
         });
 
         taskList.appendChild(li);
-        taskInput.value = "";
+        taskInput.value = ""; // Réinitialise l'input
     }
 });
